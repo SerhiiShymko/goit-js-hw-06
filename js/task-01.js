666ea5887bf979f85b5b66e286859f510849b6c2
SerhiiShymko <shimko456@gmail.com>
2023-02-16 16:53:21 +0200

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,23 @@
-const navLI = document.querySelector("li");
-console.log(navLI);
+// const totalCategory = document.querySelectorAll("li.item");
+// console.log("Number of categories:", totalCategory.length);
+
+// const list = document.li;
+// console.log(list);
+// const list1 = li.firstElementChild;
+// console.log(list);
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
+
+console.log(document);
+
+const ul = document.querySelector("#categories");
+console.log(ul);
+
+const list = ul.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
