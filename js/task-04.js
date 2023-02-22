4667c9b9ede8875ebb7f7ad191a49711613391cb
SerhiiShymko <shimko456@gmail.com>
2023-02-22 16:56:02 +0200

    task-05

--- js/task-04.js
+++ js/task-04.js
@@ -1,75 +1,69 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+let counterValue = 0;
+const decrementBtn = document.querySelector(`[data-action="decrement"]`);
+const incrementBtn = document.querySelector(`[data-action="increment"]`);
+const valueUI = document.querySelector("#value");
 
-// const decrementBtn = document.querySelector(`[data-action="decrement"]`);
-// const incrementBtn = document.querySelector(`[data-action="increment"]`);
-// const valueEl = document.querySelector("#value");
+decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  valueUI.textContent = counterValue;
+});
 
-// console.log(decrementBtn);
-// console.log(incrementBtn);
-// console.log(valueEl);
+incrementBtn.addEventListener("click", function () {
+  counterValue += 1;
+  valueUI.textContent = counterValue;
+});
+// ////////////////////////////////////////////////////////////////////////////////////////
+// const counterValue = function ({
+//   rootSelector,
+//   initialValue = 0,
+//   step = 1,
+// } = {}) {
+//   this._value = initialValue;
+//   this._step = step;
 
-const counterValue = function ({
-  rootSelector,
-  initialValue = 0,
-  step = 1,
-} = {}) {
-  this._value = initialValue;
-  this._step = step;
+//   this._refs = this._getRefs(rootSelector);
 
-  this._refs = this._getRefs(rootSelector);
+//   this._bindEvents();
+//   this.updateValueUI();
+// };
 
-  this._bindEvents();
-  this.updateValueUI();
-};
+// counterValue.prototype._getRefs = function (rootSelector) {
+//   const refs = {};
+//   refs.container = document.querySelector(rootSelector);
+//   refs.decrementBtn = refs.container.querySelector(`[data-action="decrement"]`);
+//   refs.incrementBtn = refs.container.querySelector(`[data-action="increment"]`);
+//   refs.value = refs.container.querySelector("#value");
 
-counterValue.prototype._getRefs = function (rootSelector) {
-  const refs = {};
-  refs.container = document.querySelector(rootSelector);
-  refs.decrementBtn = refs.container.querySelector(`[data-action="decrement"]`);
-  refs.incrementBtn = refs.container.querySelector(`[data-action="increment"]`);
-  refs.value = refs.container.querySelector("#value");
+//   return refs;
+// };
 
-  return refs;
-};
+// counterValue.prototype._bindEvents = function () {
+//   this._refs.incrementBtn.addEventListener("click", () => {
+//     console.log(this);
 
-counterValue.prototype._bindEvents = function () {
-  this._refs.incrementBtn.addEventListener("click", () => {
-    console.log(this);
-    this.increment();
-    this.updateValueUI();
-  });
-  this._refs.decrementBtn.addEventListener("click", () => {
-    console.log(this);
-    this.decrement();
-    this.updateValueUI();
-  });
-};
+//     this.increment();
+//     this.updateValueUI();
+//   });
+//   this._refs.decrementBtn.addEventListener("click", () => {
+//     console.log(this);
 
-counterValue.prototype.updateValueUI = function () {
-  this._refs.value.textContent = this._value;
-};
+//     this.decrement();
+//     this.updateValueUI();
+//   });
+// };
 
-counterValue.prototype.decrement = function () {
-  this._value -= this._step;
-};
-counterValue.prototype.increment = function () {
-  this._value += this._step;
-};
+// counterValue.prototype.updateValueUI = function () {
+//   this._refs.value.textContent = this._value;
+// };
 
-new counterValue({ rootSelector: "#counter", step: 1 });
+// counterValue.prototype.decrement = function () {
+//   this._value -= this._step;
+// };
+// counterValue.prototype.increment = function () {
+//   this._value += this._step;
+// };
 
-// decrementBtn.addEventListener("click", () => {
-//   counter.decrement();
-//   // console.log(counter);
-//   valueEl.textContent = counter.value;
-//   console.log(counterValue);
-// });
-
-// incrementBtn.addEventListener("click", function () {
-//   counter.increment();
-//   // console.log(counter);
-//   valueEl.textContent = counter.value;
-//   console.log(counterValue);
-// });
+// new counterValue({ rootSelector: "#counter", step: 1 });
