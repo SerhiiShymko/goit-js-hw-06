fbce912e82afd6fbc68d980d09ad1fcbeeba0313
SerhiiShymko <shimko456@gmail.com>
2023-02-26 14:09:32 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,9 @@
+const refs = {
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+};
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
