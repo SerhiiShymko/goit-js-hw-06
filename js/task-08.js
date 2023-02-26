1b8e83fa543009a2d6b0e064c90d86b19701fd32
SerhiiShymko <shimko456@gmail.com>
2023-02-26 12:45:12 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const form = document.querySelector(".login-form");
 
+const handleSuccess = () => {
+  document.querySelector("form").remove();
+  //   document
+  //     .querySelector(".login-form")
+  //     .insertAdjacentHTML("afterbegin", "<h2>Вхід успішний</h2>");
+};
+
+const handleError = () => {
+  const errorText = document.createElement("p");
+  errorText.style.color = "red";
+  errorText.textContent = "Всі поля повинні бути заповнені";
+  document.querySelector(".login-form").append(errorText);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    handleError();
+    return;
+  }
+  handleSuccess();
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", handleSubmit);
