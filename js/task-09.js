1e574bb962671bb1b429743529b02e55784134be
SerhiiShymko <shimko456@gmail.com>
2023-02-27 09:26:18 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,7 +3,9 @@ const btnColor = document.querySelector(".change-color");
 const changedColors = document.querySelector(".color");
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 btnColor.addEventListener("click", () => {
